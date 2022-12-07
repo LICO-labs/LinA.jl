f1d38f6625aafba8ee616d4e05a3e17216cd38cb
Julien Codsi <julien.codsi@umontreal.ca>
2022-12-07 15:33:04 +0000

    added support for CPLEX breakpoints and continuity checker

--- src/LinA.jl
+++ src/LinA.jl
@@ -8,10 +8,12 @@ using GeneralizedGenerated
 
 export Linearize , LinearBounding
 export Absolute, Relative, Best, Under, Over, HeuristicLin, ExactLin
+export isContinuous, CplexBreakpoints
 
 include("strucDef.jl")
 include("Bounding.jl")
 include("Linearize Dispatch.jl")
+include("Utilities.jl")
 #include("ORourke.jl")
 #include("exact method.jl")
 
