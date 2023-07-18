fcbd2cfa30c9234f95195b466297478a486ea5b7
Julien Codsi <julien.codsi@umontreal.ca>
2023-07-18 15:22:34 +0200

    renamed breakpoint function and made the doc clearer

--- src/utilities.jl
+++ src/utilities.jl
@@ -30,12 +30,12 @@ end
 
 
 """
-CplexBreakpoints(pwl, ε = 1e-5)
+Breakpoints(pwl, ε = 1e-5)
 
-Outputs the breakpoints needed for CPLEX to natively model PWL functions.
+Outputs the breakpoints needed for several solvers (CPLEX, Gurobi,...) to natively model PWL functions. 
 # Arguments
 - `plw` : pwl function
-- `ε` : numerical precision
+- `ε` : numerical precision used to detect if the end endpoints of two segments are equal (to detect discontinuities)
 
 """
 function CplexBreakpoints(pwl, ε = 1e-5) 
