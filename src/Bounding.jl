5f8900d38ef8171220fcd5ea6e22719cc3d1ca4f
Julien Codsi <37729872+Codsilla@users.noreply.github.com>
2024-02-09 18:41:43 -0500

    fix bug where linear bounding wouldn't do the exact algorithm

--- src/Bounding.jl
+++ src/Bounding.jl
@@ -2,14 +2,27 @@
 include("Linearize Dispatch.jl")
 include("exact method.jl")
 
-function LinearBounding(expr_fct::Ef,x1::Real,x2::Real, e::Absolute;
+
+#to dispatch
+function LinearBounding(expr_fct::Ef,x1::Real,x2::Real, e::ErrorType;
+    ConcavityChanges = [Inf]::Array{Float64,1} )
+    
+    LinearBounding(expr_fct,x1,x2, e , HeuristicLin());ConcavityChanges = ConcavityChanges  )
+    
+end
+
+
+function LinearBounding(expr_fct::Ef,x1::Real,x2::Real, e::Absolute,algorithm;
     ConcavityChanges = [Inf]::Array{Float64,1} )
 
-under = Linearize(expr_fct,x1,x2, e; bounding = Under(),ConcavityChanges = ConcavityChanges )
+under = Linearize(expr_fct,x1,x2, e; bounding = Under(),algorithm,ConcavityChanges = ConcavityChanges )
 
 return under, under + 2*e.delta
 
 end
+
+
+
 
 """
     LinearBounding(expr_fct::Ef,x1::Real,x2::Real, e::ErrorType; ConcavityChanges = [Inf]::Array{Float64,1} )
@@ -24,11 +37,15 @@ Makes an optimal piecewise Linear underestimation and overestimation of expr_fct
 # Optional Arguments
 - `ConcavityChanges` : Concavity changes in the function. If not given, they will be computed automatically which, in rare cases, can lead to precision errors if the concavity is asymptotic to zero. 
 
+!!! note
+    It is also possible to specify which algorithm to use between `HeuristicLin()` and `ExactLin()` by simply adding it after the error type.
+    By default LinA uses the heuristic.
+
 """
-function LinearBounding(expr_fct::Ef,x1::Real,x2::Real, e::ErrorType; ConcavityChanges = [Inf]::Array{Float64,1} )
+function LinearBounding(expr_fct::Ef,x1::Real,x2::Real, e::ErrorType,algorithm; ConcavityChanges = [Inf]::Array{Float64,1} )
     
-lower = Linearize(expr_fct,x1,x2, e; bounding = Under(),ConcavityChanges = ConcavityChanges )
-upper = Linearize(expr_fct,x1,x2, e; bounding = Over(),ConcavityChanges = ConcavityChanges )
+lower = Linearize(expr_fct,x1,x2, e; bounding = Under(),algorithm,ConcavityChanges = ConcavityChanges )
+upper = Linearize(expr_fct,x1,x2, e; bounding = Over(),algorithm,ConcavityChanges = ConcavityChanges )
 return lower,upper 
     
 end
